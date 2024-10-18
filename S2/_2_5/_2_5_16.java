0e7c178a474b3487cb094a90bd27a2d7cbc5860e
Efe Yencilek <64659675+FarukEfe@users.noreply.github.com>
2024-10-17 20:41:04 -0400

    Start writing _2_5_16.java

--- S2/_2_5/_2_5_16.java
+++ S2/_2_5/_2_5_16.java
@@ -2,4 +2,8 @@ package S2._2_5;
 
 public class _2_5_16 {
     
+    // Make an array with the specified order in the question
+    // Make a comparison algorithm according to the order of the above array
+    // Use this to sort a list of strings (apply merge sort, heap sort, quick sort, etc.)
+
 }
