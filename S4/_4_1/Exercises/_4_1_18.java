3f730e054374d4276738678a27038648bac6a8ed
Efe Yencilek <64659675+FarukEfe@users.noreply.github.com>
2024-12-09 18:18:53 -0500

    Write new questions about graphs

--- S4/_4_1/Exercises/_4_1_18.java
+++ S4/_4_1/Exercises/_4_1_18.java
@@ -1,5 +1,10 @@
 package S4._4_1.Exercises;
 
+// Solution: Run dfs from each node and find itself back. 
+// Have global variable smallestSize to track smallest cycle size.
+// Recursively keep a count 'size' and reset size = 0 after each dfs() iteration
+// If a path back to itself exists and size < smallestSize, update smallestSize = size
+// Else, continue dfs until it finishes
 public class _4_1_18 {
     
 }
